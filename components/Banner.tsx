384ccef51df3989dd73bb04b610413e092e28b2f
zibby-zibs <oloyedetimi@gmail.com>
2023-03-06 21:16:51 +0100

    fixed banner padding

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -23,7 +23,7 @@ function Banner({netflixOriginals}: Props) {
     }, [netflixOriginals])
 
   return (
-    <main className="flex flex-col space-y-2 py-20 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+    <main className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
         <Image
           alt=""
